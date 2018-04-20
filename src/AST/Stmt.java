385063b04f7239d356be1ec6da81000def58fcf4
lucasyoshimura <lucasrigoyoshimura@gmail.com>
2018-04-20 20:55:15 -0300

    genC do stmt e stmt_list

--- src/AST/Stmt.java
+++ src/AST/Stmt.java
@@ -2,9 +2,27 @@ package AST;
 
 import Lexer.Symbol;
 
-public abstract class Stmt {
-    abstract public void genC(PW pw);
-    //abstract Symbol setTipo();
-
-
+public class Stmt {
+    private Symbol tipo;
+    private Read_stmt read;
+    private Assign_expr assign;
+    private Write_stmt write;
+    private Return_stmt ret;
+    private If_stmt if_stmt;
+    private For_stmt for_stmt;
+   
+    public void genC(PW pw){
+       if(tipo.equals(Symbol.READ))
+            read.genC(pw);
+       else if(tipo.equals(Symbol.ASSIGN))
+            assign.genC(pw);
+       else if(tipo.equals(Symbol.WRITE))
+            write.genC(pw);
+       else if(tipo.equals(Symbol.RETURN))
+            ret.genC(pw);
+       else if(tipo.equals(Symbol.IF))
+            if_stmt.genC(pw);
+       else if(tipo.equals(Symbol.FOR))
+            for_stmt.genC(pw);
+   } 
 }
--- src/AST/Stmt_list.java
+++ src/AST/Stmt_list.java
@@ -6,12 +6,6 @@ import java.util.ArrayList;
 
 public class Stmt_list extends Stmt{
     private ArrayList<Stmt> stmtlist;
-    private Read_stmt read;
-    private Assign_expr assign;
-    private Write_stmt write;
-    private Return_stmt ret;
-    private If_stmt if_stmt;
-    private For_stmt for_stmt;
     
 
     public Stmt_list(ArrayList<Stmt> stmtlist) {
@@ -25,18 +19,7 @@ public class Stmt_list extends Stmt{
     public void genC(PW pw) {
         if(stmtlist != null){
             for(Stmt s : stmtlist) {
-                if(s.equals(read))
-                    read.genC(pw);
-                else if(s.equals(assign))
-                    assign.genC(pw);
-                else if(s.equals(write))
-                    write.genC(pw);
-                else if(s.equals(ret))
-                    ret.genC(pw);
-                else if(s.equals(if_stmt))
-                    if_stmt.genC(pw);
-                else if(s.equals(for_stmt))
-                    for_stmt.genC(pw);
+                s.genC(pw);
             }
         }
     }
