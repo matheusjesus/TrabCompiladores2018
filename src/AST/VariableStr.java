e2125788279e5439dc17cd8b3f1f7dc724d26e61
matheus jesus <matheus.s.jesus14@gmail.com>
2018-05-03 15:27:34 -0300

    VariableStr agora herda Var_type

--- src/AST/VariableStr.java
+++ src/AST/VariableStr.java
@@ -1,27 +1,24 @@
 package AST;
         
-public class VariableStr {
-    private String nome;
+import Lexer.Symbol;
+
+public class VariableStr extends Var_type{
     private String str;
     /* nome (id) = A
     str = STRINGLITERAL*/
     public VariableStr(String nome, String str){
-        this.nome = nome;
+        super(nome, Symbol.STRING);
         this.str = str;
     }
     
     public String getStr(){
         return str;
     }
-    
-    public String getNome(){
-        return nome;
-    }
     /*genC VariableStr
     declara a string como ponteiro, aloca o tamanho com malloc e copia o valor com strcpy*/
     public void genC(PW pw) {
-      pw.println("" + this.nome + " = (char *) malloc(sizeof(char) * "+this.str.length()+"));", true);
-      pw.println("strcpy(" + this.nome + ",  \"" + this.str + "\");", true);
+      pw.println("" + this.getNome() + " = (char *) malloc(sizeof(char) * "+this.str.length()+"));", true);
+      pw.println("strcpy(" + this.getNome() + ",  \"" + this.str + "\");", true);
       pw.print("\n", false);
     }
 }
